e087b431e8ae764a302382773ea1f842c8e20381
Kei Kori <kori@aucfan.com>
2014-05-28 11:08:50 +0900

    title,date抽出追加

--- auc_topic.sh
+++ auc_topic.sh
@@ -2,19 +2,24 @@
 wait=1s
 
 # 記事ページ一覧を引き抜き
-pages=`wget -q http://aucfan.com/article/ -O - | grep -Po '(?<=<a href=")http://.*/(?=" class="box_link">)'`
+pages=`wget -q http://aucfan.com/article/ -O - | grep -Po '(?<=<a href=")http:\/\/aucfan\.com\/article\/.*?\/(?=" class="box_link">)'`
 echo 'URL一覧：'
-echo $pages
+echo -e "$pages"
+echo
 sleep $wait
 
 # 記事をwget
-echo
-echo 'wget実行...'
 IFS=$'\n' # 行単位で分割
 for url in $pages; do
     echo "wget実行：$url"
     html=`wget -q $url -O -`
-# echo $html
+
+    title=`echo $html | grep -Po '(?<=<h1 class="entry-title">).*?(?=<\/h1>)'`
+    date=`echo $html | grep -Po '(?<=<span class="sep">).*?(?=<\/span>)' | sed -Ee 's/[年月]/\//g' -e 's/日//g' -e 's/\/([0-9])\//\/0\1\//g'`
+
+    echo "url:$url"
+    echo "title:$title"
+    echo "date:$date"
 
     # LTSV形式で保存
 
