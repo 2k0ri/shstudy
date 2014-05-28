e11a1091c139717e3b93e40b1251410bae0f325d
Kei Kori <kori@aucfan.com>
2014-05-28 12:49:53 +0900

    contentパースを追加

--- auc_topic.sh
+++ auc_topic.sh
@@ -16,15 +16,18 @@ for url in $pages; do
 
     title=`echo $html | grep -Po '(?<=<h1 class="entry-title">).*?(?=<\/h1>)'`
     date=`echo $html | grep -Po '(?<=<span class="sep">).*?(?=<\/span>)' | sed -Ee 's/[年月]/\//g' -e 's/日//g' -e 's/\/([0-9])\//\/0\1\//g'`
+    content=`echo $html | grep -oP "(?<=<br class='wp_social_bookmarking_light_clear' \/>)[\s\S]*(?=<div class=\"social4i\" style=\"height:69px;\">)"`
 
     echo "url:$url"
     echo "title:$title"
     echo "date:$date"
+    echo "content:"
+    echo -e "$content"
+    echo
 
     # LTSV形式で保存
 
-    # 負荷軽減のため１秒sleep
-    sleep $wait
+    sleep $wait # 負荷軽減のため1秒sleep
 done
 
 # tsv変換→SQL実行
